ecd3bd83c22f96539f45123c2caba7d63a61350f
sowmyath012 <sowmyath012@gmail.com>
2023-08-28 08:21:29 +0530

    firstcommit

---
+++ src/test/java/one/Goibbibo.java
@@ -0,0 +1,15 @@
+package one;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Goibbibo {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.goibibo.com/");
+        
+		//first push
+	}
+
+}
